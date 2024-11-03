e7298859a94199a3e9283b9ae1a9dda6612e6c42
Shiraz <smcclennon@protonmail.com>
2024-11-03 23:55:56 +0000

    fix: Improve formatting

--- bot/config.py
+++ bot/config.py
@@ -10,16 +10,16 @@ logger = logging.getLogger(__name__)
 
 VERSION = '2.0.0-dev'
 
-BOT_TOKEN = env('BOT_TOKEN')
+BOT_TOKEN         = env('BOT_TOKEN')
 DEVELOPER_CHAT_ID = env('DEVELOPER_CHAT_ID')
 
-SENTRY_DSN = env('SENTRY_DSN')
-ENVIRONMENT = env('ENVIRONMENT', default='undefined').lower()
-DEBUG = env('DEBUG', default=False, cast=bool)
+SENTRY_DSN        = env('SENTRY_DSN')
+ENVIRONMENT       = env('ENVIRONMENT', default='undefined').lower()
+DEBUG             = env('DEBUG', default=False, cast=bool)
 
-DB_NAME = env('DB_NAME')
-DB_USER = env('DB_USER')
-DB_PASSWORD = env('DB_PASSWORD')
-DB_HOST = env('DB_HOST')
-DB_PORT = env('DB_PORT')
-DB_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+DB_NAME           = env('DB_NAME', default='songid_db')
+DB_USER           = env('DB_USER', default='songid_user')
+DB_PASSWORD       = env('DB_PASSWORD')
+DB_HOST           = env('DB_HOST', default='localhost')
+DB_PORT           = env('DB_PORT', default='5432')
+DB_URL            = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
